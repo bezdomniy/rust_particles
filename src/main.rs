c9d6723481f528accb3886a13e5f69f47e7223ea
Ilia <ilia.shibaev@gmail.com>
2023-12-29 22:44:49 +1100

    make browser work too

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[cfg(not(target_arch = "wasm32"))]
 fn main() {
-    tracing_subscriber::fmt::init();
+    env_logger::init();
     let options = eframe::NativeOptions::default();
 
     let _ = eframe::run_native(
@@ -12,21 +12,19 @@ fn main() {
 
 #[cfg(target_arch = "wasm32")]
 fn main() {
-    // Make sure panics are logged using `console.error`.
-    console_error_panic_hook::set_once();
-
-    // Redirect tracing to console.log and friends:
-    tracing_wasm::set_as_global_default();
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
 
     let options = eframe::WebOptions::default();
 
     wasm_bindgen_futures::spawn_local(async {
-        eframe::start_web(
-            "canvas",
-            options,
-            Box::new(|cc| Box::new(rust_particles::App::new(cc))),
-        )
-        .await
-        .expect("failed to start eframe");
+        eframe::WebRunner::new()
+            .start(
+                "canvas",
+                options,
+                Box::new(|cc| Box::new(rust_particles::App::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
     });
 }
